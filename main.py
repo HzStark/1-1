8c9232d1b9d7b97e218e8672f39a9e1dc4c6a55b
HzStark <72388591+HzStark@users.noreply.github.com>
2022-03-08 22:26:25 +0300

    f
    f

---
+++ main.py
@@ -0,0 +1,7 @@
+import time
+dosyad=input("İlk başta ki dosyanın adı: ")
+while True:
+ if dosyad=="0_0":
+  print("Keys: 'https://www.youtube.com/watch?v=uDeTc8ee8lc',Keys.ENTER")
+  time.sleep(5)
+  break
