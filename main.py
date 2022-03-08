247876e0b97c91d312ab6173941cf958445df6e8
HzStark <72388591+HzStark@users.noreply.github.com>
2022-03-08 22:34:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import time
 dosyad=input("İlk başta ki dosyanın adı: ")
 while True:
  if dosyad=="0_0":
-  print("Keys: 'https://www.youtube.com/watch?v=uDeTc8ee8lc',Keys.ENTER")
+  print("https://www.youtube.com/watch?v=uDeTc8ee8lc")
   time.sleep(5)
   break
